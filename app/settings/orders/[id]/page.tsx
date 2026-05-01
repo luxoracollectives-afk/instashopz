1b159cb95648700057f9543b1f12840500bfb5cf
David <luxoracollectives@gmail.com>
2026-05-01 22:42:49 +0530

    fix: correct absolute path for vercel

--- app/settings/orders/[id]/page.tsx
+++ app/settings/orders/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { use } from "react";
-import { orders } from "../../../data/orders";
+import { orders } from "@/app/data/orders";
 
 export default function OrderDetails({
   params,
