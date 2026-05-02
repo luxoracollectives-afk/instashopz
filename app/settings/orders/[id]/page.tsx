1437f55f9c743a4d394858c053a2127c07544d3a
David <luxoracollectives@gmail.com>
2026-05-02 13:04:01 +0530

    force fresh deploy

--- app/settings/orders/[id]/page.tsx
+++ app/settings/orders/[id]/page.tsx
@@ -1,8 +1,9 @@
-"use client";
+// force fresh deploy
+ "use client";
 
 import { useRouter } from "next/navigation";
 import { use } from "react";
-import { orders } from "@/app/data/orders";
+import { orders } from "../../../data/orders";
 
 export default function OrderDetails({
   params,
